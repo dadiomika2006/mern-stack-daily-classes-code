d0bbfb5f8b3fa69135d78f53c1a08c06345b9375
Omika <dadiomika@gmail.com>
2026-05-07 23:24:14 +0530

    day-14 code

--- Products.jsx
+++ Products.jsx
@@ -1,36 +1,9 @@
-import React from "react";
-import ProductCard from "./ProductCard";
+import React from 'react'
 
-const Products=()=>{
-    let products=[{Name:"Mobile",
-        Price:20000,
-        Description:"Good Condition",
-        Ratings:"⭐⭐⭐",
-        imageSrc:"https://darlingretail.com/cdn/shop/files/iPhone_15_Blue_Pure_Back_iPhone_15_Blue_Pure_Front_2up_Screen__WWEN.jpg?v=1695103868"
-    },
-        {
-            Name:"Laptop",
-            Price:50000,
-            Description:"Excellent Condition",
-            Ratings:"⭐⭐⭐⭐",
-            imageSrc:"https://m.media-amazon.com/images/I/71Hy5SYr3tL._AC_UF1000,1000_QL80_.jpg"
-        },
-        {Name:"Headpphones",
-            Price:2000,
-            Description:"Perfect condition",
-            Ratings:"⭐⭐⭐⭐",
-            imageSrc:"https://m.media-amazon.com/images/I/610ub5kytVL.jpg"
-        }
-    ];
-    return ( <div id="main-container">
-            {products.map((products)=>(
-            <div>
-                <ProductCard details={products}/>
-            </div>
-            ))}
-        </div>
-    );
-       
+const Products = () => {
+  return (
+    <div>Products page</div>
+  )
 }
 
-export default Products;
+export default Products
